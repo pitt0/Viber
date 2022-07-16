d7ea1e9cc103eeb8505e8afc79b9d497531b9f11
Samuele Baffo <samuelebaffo32@gmail.com>
2022-07-16 16:01:57 +0200

    Added new commands

--- groups/songs.py
+++ groups/songs.py
@@ -1,31 +1,18 @@
 from discord import app_commands as slash
 
 import discord
-import json
 
-from connections import SongCache
-from models import Advices, Song
-from models import song as songs
+from models import Advices, choose
 
 class Songs(slash.Group):
     
     @slash.command(name='advice', description='Advices a song to someone in this server')
     async def advice_song(self, interaction: discord.Interaction, song: str, to: discord.Member) -> None:
         await interaction.response.defer()
-
-        song_id: str | int
-
-        with SongCache() as cache:
-            song_id = cache.get(song) # type: ignore
-        
-        if song_id is not None:
-            _song = Song.from_id(song_id)
-        else:        
-            _song = await songs.choose(interaction, song)
-            song_id = _song.id
-
         advices = Advices.from_database(to)
-            
+        _song = await choose(interaction, song)
+
+
         if _song not in advices.songs:
             advices.add_song(_song)
             message = f"`{_song.title} • {_song.author}` adviced to {to.mention}"
@@ -38,4 +25,7 @@ class Songs(slash.Group):
 
     @slash.command(name='search')
     async def search_song(self, interaction: discord.Interaction, song: str) -> None:
-        pass
+        await interaction.response.defer()
+        _song = await choose(interaction, song)
+
+        await interaction.followup.send(embed=_song.embed)
