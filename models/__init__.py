99ae397b9a4bd8b3328d0f1f7e49838d1a465804
pitt0 <pitto.developer@gmail.com>
2023-04-08 18:17:25 +0200

    [Structure] Moved types.py out of utils and removed the folder

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,11 @@
+import json
+
 from .data import *
+from .typing import *
 
 from .playlists import *
 from .songs import *
 from .player import *
+
+with open('database/ffmpeg.json') as f:
+    FFMPEG_OPTIONS = json.load(f)
--- models/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-import json
-
-from .typing import *
-
-with open("database/ffmpeg.json") as f:
-    FFMPEG_OPTIONS = json.load(f)
