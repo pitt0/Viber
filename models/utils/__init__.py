f32aedc40ff10619117d3a1b205614753d8ff048
pitt0 <pitto.developer@gmail.com>
2023-04-08 17:33:00 +0200

    [Code] Update imports

--- models/utils/__init__.py
+++ models/utils/__init__.py
@@ -2,10 +2,11 @@ import json
 
 from .errors import *
 
-from .genius import lyrics
-
+from .genius import *
 from .spotify import *
 from .youtube import *
+
+from .typing import *
 
 with open("database/ffmpeg.json") as f:
     FFMPEG_OPTIONS = json.load(f)
